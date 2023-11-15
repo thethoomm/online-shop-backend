baa6f9a8f870bdcc55b70909a78f11aadddafb3b
Thomas Henrique <thomas.henrique@aluno.ifsp.edu.br>
2023-11-15 12:02:53 -0300

    feat:  insert cache

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
 import { CityController } from './city.controller';
 import { CityService } from './city.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  imports: [
+    CacheModule.register({
+      ttl: 900_000_000,
+    }),
+  ],
   controllers: [CityController],
   providers: [CityService, PrismaService],
 })
